97421ab28e13bd9019df8dc8cf0359f285b1bb9e
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2023-12-16 12:23:23 +0900

    Add bean lifecycle event

--- src/main/java/com/app/spring/implementation/DistributorSellerServiceImpl.java
+++ src/main/java/com/app/spring/implementation/DistributorSellerServiceImpl.java
@@ -1,6 +1,8 @@
 package com.app.spring.implementation;
 
 import com.app.spring.service.SellerService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,16 @@ import org.springframework.stereotype.Service;
 public class DistributorSellerServiceImpl implements SellerService {
     public DistributorSellerServiceImpl() {
         System.out.println("Constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void sendPostConstructNotification() {
+        System.out.println("Hey, this is post construct bean lifecycle event");
+    }
+
+    @PreDestroy
+    public void sendPreDestroyNotification() {
+        System.out.println("Hey, this is pre destory bean lifecycle event");
     }
 
     @Override
