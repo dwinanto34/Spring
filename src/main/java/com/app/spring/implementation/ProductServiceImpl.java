ae39ee4a6188b1ecbcdfb5754ed12c85e7e652c0
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2023-12-16 18:09:03 +0900

    Add JPA repository approach

--- src/main/java/com/app/spring/implementation/ProductServiceImpl.java
+++ src/main/java/com/app/spring/implementation/ProductServiceImpl.java
@@ -2,12 +2,14 @@ package com.app.spring.implementation;
 
 import com.app.spring.dao.ProductDao;
 import com.app.spring.entity.Product;
+import com.app.spring.repository.ProductRepository;
 import com.app.spring.service.ProductService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 // Service annotation is a specialization of @Component,
 // It allows for better organization and differentiation between different types of components within the application
@@ -15,39 +17,65 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
+    private ProductRepository productRepository;
 
     @Autowired
-    public ProductServiceImpl(ProductDao productDao) {
+    public ProductServiceImpl(ProductDao productDao, ProductRepository productRepository) {
         this.productDao = productDao;
+        this.productRepository = productRepository;
     }
 
     @Override
     public List<Product> findAll() {
-        return productDao.findAll();
+//        Using EntityManager
+//        return productDao.findAll();
+
+//        Using JPARepository
+        return productRepository.findAll();
     }
 
     @Override
     public Product findById(String id) {
-        return productDao.findById(id);
+//        Using EntityManager
+//        return productDao.findById(id);
+
+//        Using JPARepository
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        }
+        return null;
     }
 
     @Override
 //    Transactional -- All or Nothing
     @Transactional
     public void save(Product product) {
-        productDao.save(product);
+//        Using EntityManager
+//        productDao.save(product);
+
+//        Using JPARepository
+        productRepository.save(product);
     }
 
     @Override
     @Transactional
     public void update(Product product) {
-        productDao.update(product);
+//        Using EntityManager
+//        productDao.update(product);
+
+//        Using JPARepository
+        productRepository.save(product);
     }
 
     @Override
     @Transactional
     public void delete(String id) {
-        Product product = findById(id);
-        productDao.delete(product);
+//        Using EntityManager
+//        Product product = findById(id);
+//        productDao.delete(product);
+
+//        Using JPARepository
+        productRepository.deleteById(id);
     }
 }
---
+++ src/main/java/com/app/spring/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.app.spring.repository;
+
+import com.app.spring.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+// Previously, when using EntityManager, there was a lot of boilerplate code.
+// With JPARepository, we only need to specify the entity class and the data type of the primary key.
+// In this case, our entity is Product, and the primary key is ID, which is of type String.
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+}
