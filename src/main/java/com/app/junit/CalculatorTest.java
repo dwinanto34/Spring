9a00d9fe7c21ab8b3e74a0c8f3d21fb42e8b04cd
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-01-04 17:23:36 +0900

    Lifecycle

--- src/main/java/com/app/junit/CalculatorTest.java
+++ src/main/java/com/app/junit/CalculatorTest.java
@@ -8,6 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Execute at the beginning of each of test case");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Execute at the end of each of test case");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Execute at the beginning of the test");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Execute at the end of the test");
+    }
+
     @Test
     public void testAddSuccess() {
         System.out.println("Running testAddSuccess");
